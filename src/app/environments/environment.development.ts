cc86f81722fe11307d2f25237969c7db95d4d3a5
Vinay Patidar <vpatidar94@gmail.com>
2023-12-23 16:35:23 +0530

    env

--- src/app/environments/environment.development.ts
+++ src/app/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    BASE_API_URL: 'http://localhost:5002',
+    BASE_API_URL: 'https://scholarship-api-app-edsb2.ondigitalocean.app',
     WHATSAPP_URL: 'https://av2.wa0.in/cloud/messages',
 };
