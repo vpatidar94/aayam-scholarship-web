672125beec984607a69d40ebf3e5d44d68ae8852
Vinay Patidar <vpatidar94@gmail.com>
2023-12-23 15:29:12 +0530

    change env and script

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,7 +1,4 @@
 export const environment = {
-    BASE_API_URL: 'https://aayam-star-api-6bqic.ondigitalocean.app',
-    QUESTIONS_API_URL: 'https://api.aayamcareerinstitute.co.in/api',
+    BASE_API_URL: 'https://scholarship-api-app-edsb2.ondigitalocean.app',
     WHATSAPP_URL: 'https://av2.wa0.in/cloud/messages',
-    W_API_KEY: 'NjRkNWNiMTMzOWRkNGQ4YzQwMTlkMWQw',
-    BUCKET_URL: 'https://aayam-star-bucket.blr1.digitaloceanspaces.com'
 };
